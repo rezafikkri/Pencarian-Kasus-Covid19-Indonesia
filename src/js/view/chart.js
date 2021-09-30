5d25a405918135ae519828e13e50bb06fa08b78a
fikkrireza <fikkri.reza@gmail.com>
2021-09-30 10:20:03 +0700

    feat: chart

--- src/js/view/chart.js
+++ src/js/view/chart.js
@@ -1,46 +1,92 @@
 import ApexCharts from 'apexcharts';
 
 function renderLineChart() {
-    /* const options = {
+    const options = {
         chart: {
-            type: 'line',
+            type: 'area',
             height: 350,
             zoom: {
                 enabled: false,
             },
             toolbar: { show: false },
         },
+        title: {
+            text: 'Nasional',
+            align: 'left',
+            style: {
+                fontFamily: 'WorkSans Bold',
+                fontSize: '18px',
+                color: '#526166',
+            },
+        },
         series: [
+            {
+                name: 'Sembuh',
+                data: [2, 20, 50, 120, 600, 200, 700],
+            },
             {
                 name: 'Positif',
                 data: [120, 400, 300, 500, 900, 700, 1000],
             },
             {
-                name: 'Sembuh',
-                data: [10, 20, 50, 120, 600, 200, 700],
+                name: 'Meniggal',
+                data: [2, 10, 20, 40, 100, 200, 500],
             },
         ],
+        labels: ['16 Nov', '17 Nov', '18 Nov', '19 Nov', '20 Nov', '21 Nov', '22 Nov'],
+        colors: ['#2A9D8F', '#E9C46A', '#E76F51'],
+        fill: {
+            gradient: {
+                opacityFrom: .7,
+                opacityTo: 0,
+            },
+        },
         dataLabels: {
             enabled: false,
         },
         stroke: {
             curve: 'smooth',
+            width: 3,
         },
-        labels: ['16 Nov', '17 Nov', '18 Nov', '19 Nov', '20 Nov', '21 Nov', '22 Nov'],
+        xaxis: {
+            labels: {
+                style: {
+                    colors: '#78909c',
+                    fontFamily: 'WorkSans Regular',
+                },
+            },
+        },
+        yaxis: {
+            labels: {
+                style: {
+                    colors: '#78909c',
+                    fontFamily: 'WorkSans Regular',
+                },
+            },
+        },
         legend: {
-            fontFamily: 'Nunito Regular',
+            fontFamily: 'WorkSans Regular',
             markers: {
                 radius: 4,
             },
         },
         grid: {
             borderColor: '#e8e8e8',
-            strokeDashArray: 4,
+            xaxis: {
+                lines: {
+                    show: true,
+                },
+            },
+        },
+        tooltip: {
+            style: {
+                fontFamily: 'WorkSans Regular',
+            },
         },
     };
 
-    const chart = new ApexCharts(document.querySelector(".chart"), options);
-    chart.render(); */
+    const chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
 }
 
 export default renderLineChart;
